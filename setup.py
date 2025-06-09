a7c84820eaf2686ba228b1f33c718b7f32532a61
Ezeana Micheal <michael.c.ezeana@gmail.com>
2025-06-09 19:05:15 +0100

    minor changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ai_api_test_docs",
-    version="0.1.2",
+    version="0.1.4",
     description="AI-powered API test and documentation generator using Gemini",
     long_description=long_description,
     long_description_content_type="text/markdown",
